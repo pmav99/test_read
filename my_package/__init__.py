c3537e3069285adc0b7435c8f3d5b08e91c3079f
Panos Mavrogiorgos <pmav99@gmail.com>
2021-03-27 01:07:23 +0200

    Update package

---
+++ my_package/__init__.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from typing import Optional
+
+
+def my_func(a: int, b: str, c: Optional[str] = None) -> None:
+    """
+    Return nothing
+
+    Args:
+        a: Just an integer
+        b: Just a string
+        c: An optional string
+
+    """
+    np.max
